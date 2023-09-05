0e6fcad0f752515a63d330f3ce4d57f027c90e8e
RijaaShamsuddin <RijaaShamsuddin@users.noreply.github.com>
2023-09-05 18:32:36 +0100

    [Prod] Staging to master 1 (#17)
    * my first commit (#1)
    
    Signed-off-by: RijaaShamsuddin <rijaa04@gmail.com>
    
    * [PR] - develop to staging 1 (#16)
    
    * my first commit
    
    Signed-off-by: RijaaShamsuddin <rijaa04@gmail.com>
    
    * [Issue 3] - Delete Weather API (#14)
    
    * deleted weather api
    
    * Weather class deleted
    
    * [Issue 2] - Implement UI (#4)
    
    changes in react code
    
    ---------
    
    Signed-off-by: RijaaShamsuddin <rijaa04@gmail.com>
    
    ---------
    
    Signed-off-by: RijaaShamsuddin <rijaa04@gmail.com>

---
+++ TDD/TDD.Tests/CalculatorTestCases.cs
@@ -0,0 +1,55 @@
+using TDD.Logics;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace TDD.Tests
+{
+    [TestClass]
+    public class CalculatorTestCases
+    {
+        private ICalculator _calculator = new Calculator();
+
+        [TestMethod]
+        public void Should_Add_Number()
+        {
+            int actual = _calculator.Add(5, 5);
+            int expected = 10;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Should_Subtract_Number()
+        {
+            int actual = _calculator.Subtract(10, 5);
+            int expected = 5;
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Should_Multiply_Number()
+        {
+            int actual = _calculator.Multiply(5, 5);
+            int expected = 25;
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Should_Divide_Number()
+        {
+            int actual = _calculator.Divide(10, 5);
+            int expected = 2;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void Should_Throw_Error_For_Dividing_By_Zero()
+        {
+            int actual = _calculator.Divide(10, 0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Should_Divide_Zero_By_Zero_Return_Infinity()
+        {
+            int actual = _calculator.Divide(0, 0);
+        }
+    }
+}
