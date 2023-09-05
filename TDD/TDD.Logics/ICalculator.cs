60566a9f1010884ac497e6f70f5657eeb2e0fa02
RijaaShamsuddin <RijaaShamsuddin@users.noreply.github.com>
2023-09-05 16:31:15 +0100

    my first commit (#1)
    Signed-off-by: RijaaShamsuddin <rijaa04@gmail.com>

---
+++ TDD/TDD.Logics/ICalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD.Logics
+{
+    public interface ICalculator
+    {
+        public int Add(int x, int y);
+        public int Subtract(int x, int y);
+        public int Multiply(int x, int y);
+        public int Divide(int x, int y);
+    }
+}
---
+++ TDD/TDD.WebApplication/Controllers/CalculatorAPIController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TDD.Logics;
+
+namespace TDD.WebApplication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorAPIController : ControllerBase
+    {
+        private readonly ICalculator _calculator;
+
+        public CalculatorAPIController()
+        {
+            _calculator = new Calculator();
+        }
+
+        [HttpGet("api/calculator/add/{num1}/{num2}")]
+        public IActionResult GetCalculatorAdd(int num1, int num2)
+        {
+            int result = _calculator.Add(num1,num2);
+            return Ok(result);
+        }
+
+        [HttpGet("api/calculator/subtract/{num1}/{num2}")]
+        public IActionResult GetCalculatorSubtract(int num1, int num2)
+        {
+            int result = _calculator.Subtract(num1, num2);
+            return Ok(result);
+        }
+
+        [HttpGet("api/calculator/multiply/{num1}/{num2}")]
+        public IActionResult GetCalculatorMultiply(int num1, int num2)
+        {
+            int result = _calculator.Multiply(num1, num2);
+            return Ok(result);
+        }
+
+        [HttpGet("api/calculator/divide/{num1}/{num2}")]
+        public IActionResult GetCalculatorDivide(int num1, int num2)
+        {           
+            try
+            {
+                int result = _calculator.Divide(num1, num2);
+                return Ok(result);
+            }
+            catch (DivideByZeroException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
