db814196eba47f71bcf3df66c1d69123615c03cb
RijaaShamsuddin <rijaa04@gmail.com>
2023-09-05 16:18:41 +0100

    my first commit
    Signed-off-by: RijaaShamsuddin <rijaa04@gmail.com>

---
+++ TDD/TDD.Logics/Calculator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD.Logics
+{
+    public class Calculator : ICalculator
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+
+        public int Divide(int x, int y)
+        {
+            try
+            {
+                if (x == 0 && y == 0)
+                {
+                    throw new ArgumentException("Zero divide by zero is Infinity");
+                }
+                return x / y;
+            }
+
+            catch (ArgumentException)
+            {
+                throw;
+            }
+            catch (DivideByZeroException)
+            {
+                throw;
+            }
+
+        }
+
+        public int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+
+
+    }
+}
